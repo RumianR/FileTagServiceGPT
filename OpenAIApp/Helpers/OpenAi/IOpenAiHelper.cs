bb8b6caad3eaee0889339b936b0ced5e1b4fb4fa
Naimur Rashid <RashidN96@gmail.com>
2023-11-12 01:48:27 -0500

    cleaned

--- OpenAIApp/Helpers/OpenAi/IOpenAiHelper.cs
+++ OpenAIApp/Helpers/OpenAi/IOpenAiHelper.cs
@@ -2,9 +2,6 @@
 {
     public interface IOpenAiHelper
     {
-        Task<string> CompleteSentence(string text);
-
-        Task<string> CompleteSentenceAdvanced(string text);
 
         Task<string> GetTags(string text);
     }
--- OpenAIApp/Helpers/OpenAi/OpenAiHelper.cs
+++ OpenAIApp/Helpers/OpenAi/OpenAiHelper.cs
@@ -12,10 +12,7 @@ namespace OpenAIApp.Helpers.OpenAi
 
         private readonly string _queryBase =
             "Return me in JSON format, and it absolutely must be in JSON, "
-            +
-            //"If you can't reply json, " +
-            //"just reply with -1 so I can consider this is an error." +
-            "a list of items called tags that can categorize the text with the top 50 more meaningful classifiers."
+            + "a list of items called tags that can categorize the text with the top 50 more meaningful classifiers."
             + "This text is from a pdf document or a mail letter and the purpose is to have helpful keywords/tags that are meaningful "
             + "to associate this document in a bin."
             + "Here is the document text: ";
@@ -25,31 +22,6 @@ namespace OpenAIApp.Helpers.OpenAi
             _openAiConfig = openAiConfig;
         }
 
-        public async Task<string> CompleteSentence(string text)
-        {
-            var api = new OpenAI_API.OpenAIAPI(_openAiConfig.Key);
-
-            var result = await api.Completions.GetCompletion(text);
-
-            return result;
-        }
-
-        public async Task<string> CompleteSentenceAdvanced(string text)
-        {
-            var api = new OpenAI_API.OpenAIAPI(_openAiConfig.Key);
-
-            var results = await api.Chat.CreateChatCompletionAsync(
-                new ChatRequest()
-                {
-                    Model = Model.ChatGPTTurbo,
-                    Temperature = 0.1,
-                    //MaxTokens = 50,
-                    Messages = new ChatMessage[] { new ChatMessage(ChatMessageRole.User, text) }
-                }
-            );
-
-            return results.Choices[0].Message.Content;
-        }
 
         public async Task<string> GetTags(string text)
         {
